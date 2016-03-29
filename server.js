a347471b8634a3ca93da2817f139ebff8e64d5d3
jaime <garciadiazjaime@gmail.com>
2016-03-29 15:14:11 -0500

    enable post

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/att', function (req, res) {
    res.status(202).send('success');
-   console.log('hello att', new Date());
+   console.log('GET hello att', new Date());
+});
+
+app.post('/att', function (req, res) {
+   res.status(202).send('success');
+   console.log('POST hello att', new Date());
+   console.log('req.body', req.body);
 });
 
 app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
